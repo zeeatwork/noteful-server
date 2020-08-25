8e4f9762b0d9f82057e2eab9e8319174e5729d23
Zenzi Ali <zeeathome935@gmail.com>
2020-08-25 17:33:53 -0400

    fixed serializer

--- src/Notes/notes-router.js
+++ src/Notes/notes-router.js
@@ -2,16 +2,17 @@ const path = require("path");
 const express = require("express");
 const xss = require("xss");
 const NotesService = require("./notes-service");
+const foldersRouter = require("../Folders/folders-router");
 
 const notesRouter = express.Router();
 const jsonParser = express.json();
 
 const serializeNotes = (note) => ({
-  id: note.id,
-  note_details: xss(note.note_details),
-  note_title: xss(note.note_title),
-  parent_folder: xss(note.parent_folder),
-  date_created: note.date_created,
+  id: String(note.id),
+  content: xss(note.note_details),
+  name: xss(note.note_title),
+  folderId: xss(note.parent_folder),
+  modified: note.date_created,
 });
 
 notesRouter
@@ -25,8 +26,10 @@ notesRouter
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const { note_title, note_details, parent_folder } = req.body;
-    const newNote = { note_title, note_details };
+    const { name , content , folderId } = req.body;
+    const newNote = { note_title: name, 
+            note_details: content,
+            parent_folder: folderId};
 
     for (const [key, value] of Object.entries(newNote)) {
       if (value == null) {
@@ -36,8 +39,6 @@ notesRouter
       }
     }
 
-    newNote.note_title = note_title;
-    newNote.note_details = note_details;
 
     NotesService.insertNote(req.app.get("db"), newNote)
       .then((note) => {
