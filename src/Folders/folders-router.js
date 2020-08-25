ee960b9bc527402c639b97731f6cc4aace5b9673
Zenzi Ali <zeeathome935@gmail.com>
2020-08-25 12:32:56 -0400

    created router and service folders

---
+++ src/Folders/folders-router.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const FoldersService = require("./folders-service");
+
+const foldersRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeFolder = (folder) => ({
+  id: folder.id,
+  folder_name: xss(folder.folder_name),
+  date_published: folder.date_published,
+});
+
+folderRouter
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    FoldersService.getAllFolders(knexInstance)
+      .then((users) => {
+        res.json(users.map(serializeUser));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const newFolder = { folder_name };
+
+    for (const [key, value] of Object.entries(newFolder)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+
+    newFolder.folder_name = folder_name;
+  
+    FoldersService.insertFolders(req.app.get("db"), newFolder)
+      .then((folder) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializeUser(folder));
+      })
+      .catch(next);
+  });
+
+foldersRouter
+  .route("/:folder_id")
+  .all((req, res, next) => {
+    FoldersService.getById(req.app.get("db"), req.params.folder_id)
+      .then((folder) => {
+        if (!folder) {
+          return res.status(404).json({
+            error: { message: `Folder doesn't exist` },
+          });
+        }
+        res.folder = folder;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeFolder(res.folder));
+  })
+  .delete((req, res, next) => {
+    FoldersService.deleteFolder(req.app.get("db"), req.params.folder_id)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const folderToUpdate = { folder_name };
+
+    const numberOfValues = Object.values(folderToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain 'folder_name'`
+        },
+      });
+
+    FoldersService.updatefolder(req.app.get("db"), req.params.folder_id, folderToUpdate)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = foldersRouter;
---
+++ src/Notes/notes-router.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const NotesService = require("./notes-service");
+
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeNotes = (note) => ({
+  id: note.id,
+  note_details: xss(note.note_details),
+  note_title: xss(note.note_title),
+  parent_folder: xss(note.parent_folder),
+  date_created: note.date_created,
+});
+
+notesRouter
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    NotesService.getAllNotes(knexInstance)
+      .then((notes) => {
+        res.json(notes.map(serializeNotes));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { note_title, note_details, parent_folder } = req.body;
+    const newNote = { note_title, note_details };
+
+    for (const [key, value] of Object.entries(newNote)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+
+    newNote.note_title = note_title;
+    newNote.note_details = note_details;
+
+    NotesService.insertNote(req.app.get("db"), newNote)
+      .then((note) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(serializeNotes(note));
+      })
+      .catch(next);
+  });
+
+notesRouter
+  .route("/:note_id")
+  .all((req, res, next) => {
+    NotesService.getById(req.app.get("db"), req.params.note_id)
+      .then((note) => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` },
+          });
+        }
+        res.note = note;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeNotes(res.note));
+  })
+  .delete((req, res, next) => {
+    NotesService.deleteNote(req.app.get("db"), req.params.note_id)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { note_title, note_details} = req.body;
+    const noteToUpdate = { note_title, note_details };
+
+    const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'note_title' or 'note_details'`,
+        },
+      });
+
+    NotesService.updateNote(req.app.get("db"), req.params.note_id, noteToUpdate)
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = NotesRouter;
